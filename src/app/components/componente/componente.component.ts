479ffcf447524bcb37b50d3659979613c1d11f0e
jenniferzusukicruzvillagomez1 <a3521110355@alumno.uttehuacan.edu.mx>
2023-02-09 13:03:30 -0600

    examen

---
+++ src/app/components/componente/componente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-componente',
+    templateUrl: './componente.component.html',
+    styleUrls:['./componente.component.css']
+  }
+)
+
+export class componenteComponent {
+
+}
